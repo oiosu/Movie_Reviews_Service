e28112df2340f2ea880978ff57fbc39543b711fc
oiosu <kdlatnrud123@naver.com>
2022-09-30 11:31:33 +0900

    movie

--- pair/movie/views.py
+++ pair/movie/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Review
 
 # Create your views here.
+def index(request):
+    return render(request, "movie/index.html")
+
+
+def new(request):
+    return render(request, "movie/new.html")
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    Review.objects.create(title=title, content=content)
+    context = {
+        "title": title,
+        "content":content,
+    }
+
+    return render(request, "movie/index.html", context)
